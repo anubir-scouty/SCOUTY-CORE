b29164f64e8eaaa79552ae759f9c4fc590dc8384
Anubir Singh <eddythemeddy@gmail.com>
2017-03-03 20:53:59 -0800

    main database class

--- libs/database.php
+++ libs/database.php
@@ -4,22 +4,11 @@ class Database{
 
 	public function __construct(){
 
-		global $config_sett;
+		global $config_sett;	
 		global $con;
 
+		require('equinox/libs/helpers/MysqliDb.php');
 
-		$con = mysqli_connect($config_sett['dbhost'],$config_sett['dbuser'],$config_sett['dbpass']) or die("MySQL Error: " . mysql_error());
-		mysqli_select_db($con, $config_sett['dbname']) or die("MySQL Error: " . mysql_error());
-
-		// echo 'hi';die;
-		// $dsn = 'mysql:dbname=shakti_society;host=localhost';
-		// $user = 'root';
-		// $password = 'root';
-		//
-		// try {
-		//     $dbh = new PDO($dsn, $user, $password);
-		// } catch (PDOException $e) {
-		//     echo 'Connection failed: ' . $e->getMessage();
-		// }
+		$db = new MysqliDb($config_sett['dbhost'], $config_sett['dbuser'], $config_sett['dbpass'], $config_sett['dbname']);
 	}
 }
