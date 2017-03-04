72a2c6de91a184f14d260025cbb1aa6018c15638
Anubir Singh <eddythemeddy@gmail.com>
2017-03-04 00:22:58 -0800

    config file

---
+++ libs/config/bundle.config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	Bundle of Pages and their dependencies
+
+	@author Anubir Singh <eddythemeddy@gmail.com>
+*/
+
+return array(
+	'global_res' => array(
+		'css' => array(
+			'dist/css/scouty.core.css'
+		),
+		'js' => array(
+			'dist/js/scouty.core.js'
+		)
+	),
+);
+
+?>
