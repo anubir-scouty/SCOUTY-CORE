b97e3fc39ab86c7fed933f080ec5c29af634f1ef
Anubir Singh <eddythemeddy@gmail.com>
2017-03-04 01:00:39 -0800

    Deleted template folder

--- template/header.php
+++
@@ -1,5 +0,0 @@
-</head>
-<body class="<?=(isset($this->bodyclass) ? $this->bodyclass : 'normal-page');?>">
-    <? require 'template/topnav.phtml';?>
-    <div class="wrapper">
-      <!--  Opening of Wrapper-->
